f998fec46a8b53767d8fc3e8481ba0ce1af0afcc
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-04-05 15:00:45 +0300

    feat: add route getAllOrders

--- controllers/orders/getAllOrders.js
+++ controllers/orders/getAllOrders.js
@@ -1,26 +1,13 @@
 const { Order } = require('../../models/order');
 
 const getAllOrders = async (req, res, next) => {
-  //   try {
-  //     const id = req.userId;
-  //     const { page = 1, limit = 5 } = req.query;
-  //     const skip = (page - 1) * limit;
-  //     const orders = await Order.find(
-  //       {
-  //         owner: id,
-  //       },
-  //       '-updatedAt'
-  //     )
-  //       .sort({ timestamps: -1, createdAt: -1 })
-  //       .skip(skip)
-  //       .limit(limit);
-  //     res.json({ orders });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  res.status(200).json({
-    resp: 'Orders',
-  });
+  try {
+    const orders = await Order.find({});
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+  res.status(200).json(orders);
 };
 
 module.exports = getAllOrders;
